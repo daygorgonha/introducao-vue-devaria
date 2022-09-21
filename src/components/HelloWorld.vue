137d60309bffb0a6f735072c4606d36342014776
Dayane Gorgonha <dayanebismark18@gmail.com>
2022-09-21 09:49:47 -0300

    feat: introduction to vue language

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export default {
+    data(){
+      return {
+          helloWorld: 'Meu primeiro componente do Vue'
+      }
+    }
+  }
+</script>
+
+<template>
+    <div>
+        <h1>Texto fixo: {{helloWorld}} </h1>
+    </div>
+</template>
+
+<style>
+
+</style>
